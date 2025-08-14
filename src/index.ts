e7188fb3233a4dfc5d2ec970c90ce64e57ec9b76
Ty Busby <tybusby@gmail.com>
2025-08-13 23:40:49 -0400

    refactor

--- src/index.ts
+++ src/index.ts
@@ -14,96 +14,10 @@
  * limitations under the License.
  */
 
-import { ContentType, STATUS, StatusCode } from "./http-constants";
-import { PLAYLISTS } from "./seasons/seasons";
-
-const API_VERSION = "v1";
-const API_PATH = `/api/${API_VERSION}/seasons`;
+import { Worker } from "./worker";
 
 export default {
-    async fetch(request): Promise<Response> {
-        if (request.method === "OPTIONS") {
-            // Handle preflight OPTIONS request
-            return getResponse(STATUS.NO_CONTENT);
-        }
-
-        if (request.method !== "GET") {
-            return getResponse(
-                STATUS.METHOD_NOT_ALLOWED,
-                getError("Method not allowed")
-            );
-        }
-
-        // favicon.ico - return no content
-        const url = new URL(request.url);
-        if (url.pathname === "/favicon.ico") {
-            return getResponse(STATUS.NO_CONTENT);
-        }
-
-        // redirect if the path ends with a slash
-        if (url.pathname.length > 1 && url.pathname.endsWith("/")) {
-            const noSlash = url.pathname.slice(0, -1);
-            const newUrl = `${url.origin}${noSlash}${url.search}`;
-            return Response.redirect(newUrl, 308);
-        }
-
-        // "/api/v#/seasons/YYYY"
-        const match = url.pathname.match(`^${API_PATH}/(\\d{4})$`);
-        if (match) {
-            const year = match[1];
-            if (year in PLAYLISTS) {
-                // Season present and valid
-                return getResponse(STATUS.OK, JSON.stringify(PLAYLISTS[year]));
-            }
-
-            // Season present but invalid
-            return getResponse(
-                STATUS.BAD_REQUEST,
-                getError(`Invalid season ${year}`)
-            );
-        }
-
-        // "/api/v#/seasons"
-        if (url.pathname === `${API_PATH}`) {
-            return getResponse(
-                STATUS.OK,
-                JSON.stringify(Object.keys(PLAYLISTS))
-            );
-        }
-
-        return getResponse(STATUS.NOT_FOUND, getError("Not Found"));
+    async fetch(request, env, ctx): Promise<Response> {
+        return await new Worker(env, ctx).fetch(request);
     },
 } satisfies ExportedHandler<Env>;
-
-function getResponse(
-    status: StatusCode,
-    body: string | null = null,
-    contentType: ContentType = "application/json"
-): Response {
-    const headers = new Headers({
-        "Cache-Control": "public, max-age=86400, immutable",
-        "X-Content-Type-Options": "nosniff",
-    });
-
-    if (body) {
-        const bodyBytes = new TextEncoder().encode(body);
-        headers.set("Content-Type", contentType);
-        headers.set("Content-Length", bodyBytes.length.toString());
-    }
-
-    return new Response(body, {
-        status,
-        headers: addCorsHeaders(headers),
-    });
-}
-
-function addCorsHeaders(headers: Headers): Headers {
-    headers.set("Access-Control-Allow-Origin", "*");
-    headers.set("Access-Control-Allow-Headers", "Content-Type");
-    headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-    return headers;
-}
-
-function getError(message: string) {
-    return JSON.stringify({ error: message });
-}
