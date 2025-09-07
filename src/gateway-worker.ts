1a102f6de241aef34e244dec2762f450afa4a18b
Ty Busby <tybusby@gmail.com>
2025-09-06 23:18:40 -0400

    latest cloud-spark version

--- src/gateway-worker.ts
+++ src/gateway-worker.ts
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 
-import { CacheHandler, CorsHandler, Method, RouteWorker } from "@adonix.org/cloud-spark";
+import {
+    CacheHandler,
+    CorsHandler,
+    Method,
+    RouteWorker,
+} from "@adonix.org/cloud-spark";
 import * as v1 from "./api/v1";
 
 export class GatewayWorker extends RouteWorker {
     protected override init(): void {
-        this.addRoute(Method.GET, v1.API_PATH, v1.PodcastWorker);
+        this.route(Method.GET, v1.API_PATH, v1.PodcastWorker);
 
         this.use(new CorsHandler());
         this.use(new CacheHandler());
